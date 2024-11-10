34b8ebad4531166562d07e26c1ec42f7ce3f6e2f
Srinath <devarapallisrinath8@gmail.com>
2024-11-10 22:12:25 +0530

    working on deployment

--- webDictionary/webDictionary/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Word(models.Model):
-    word = models.CharField(max_length=255)
-    meaning = models.TextField()
-
-    def __str__(self):
-        return self.word
